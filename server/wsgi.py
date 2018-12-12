1ecfbffed6b5d5c7651f3938aad3fd6a5c5b2a7a
Suraj Kattige <suraj.kattige056@gmail.com>
2018-12-12 16:02:39 -0700

    Made several changes

--- server/wsgi.py
+++ server/wsgi.py
@@ -3,4 +3,4 @@
 from server import app
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', 5000)
+    app.run('0.0.0.0', 5000, ssl_context=('/etc/ssl/certs/server_cert.crt', '/etc/ssl/private/server_key.key'))
